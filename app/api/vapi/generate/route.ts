7058772ff0e7fcb72205c778f79cc582015a009e
Yash Gupta <yashgupta2116@gmail.com>
2025-04-20 19:19:52 +0530

    changed the generate api a little bit
    organised the file

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,11 +1,8 @@
 import { generateText } from 'ai';
 import { google } from '@ai-sdk/google';
-import { getRandomInterviewCover } from '@/lib/utils';
-import { db } from '@/firebase/admin';
 
-export async function GET() {
-  return Response.json({ success: true, data: 'THANK YOU!' }, { status: 200 });
-}
+import { db } from '@/firebase/admin';
+import { getRandomInterviewCover } from '@/lib/utils';
 
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
@@ -13,13 +10,25 @@ export async function POST(request: Request) {
   try {
     const { text: questions } = await generateText({
       model: google('gemini-2.0-flash-001'),
-      prompt: `Prepare questions for a job interview. The job roe is ${role}. The job experience level is: ${level}. The tech stack used in the job is: ${techstack}. The focus between behavioural and technical questions should lean towards: ${type}. The amount of questions required is ${amount}.Please return only the question, without any additional text. The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant. Reutrn the questions formatted like this: ["Question 1" , "Question 2" , "Quetion 3"]  Thank you! <3`,
+      prompt: `Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
+        
+        Thank you! <3
+    `,
     });
 
     const interview = {
-      role,
-      type,
-      level,
+      role: role,
+      type: type,
+      level: level,
       techstack: techstack.split(','),
       questions: JSON.parse(questions),
       userId: userid,
@@ -28,11 +37,17 @@ export async function POST(request: Request) {
       createdAt: new Date().toISOString(),
     };
 
+    console.log('userid', userid);
+
     await db.collection('interviews').add(interview);
 
     return Response.json({ success: true }, { status: 200 });
   } catch (error) {
-    console.error(error);
+    console.error('Error:', error);
     return Response.json({ success: false, error: error }, { status: 500 });
   }
+}
+
+export async function GET() {
+  return Response.json({ success: true, data: 'Thank you!' }, { status: 200 });
 }
