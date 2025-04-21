193e6cf01160c0bd7bddcad629c8c68880312349
Yash Gupta <yashgupta2116@gmail.com>
2025-04-21 15:25:00 +0530

    added the feedback info on the dashboard too

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
-const InterviewCard = ({
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
+const InterviewCard = async ({
   id,
   userId,
   role,
@@ -12,7 +13,10 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+  const feedback =
+    userId && id
+      ? await getFeedbackByInterviewId({ interviewId: id, userId })
+      : null;
   const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now()
