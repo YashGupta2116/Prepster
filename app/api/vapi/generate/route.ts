b7cccc9ccc624627a35b4b11cb03dce9cf86f294
Yash Gupta <158163628+YashGupta2116@users.noreply.github.com>
2026-07-13 18:02:53 +0530

    fix1: trying to log the body coming

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -6,6 +6,9 @@ import { adminDb as db } from "@/firebase/admin";
 import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
+  const body = await request.json();
+
+  console.log(body);
   const { type, role, level, techstack, amount, userid } = await request.json();
 
   try {
